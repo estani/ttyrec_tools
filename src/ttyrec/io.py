65d0d0ff202f76428c9b520a6d40e9fdd7ee4818
estani@tux14 <estanislao.gonzalez@met.fu-berlin.de>
2013-02-05 11:01:34 +0100

    some small bugfixes and cleaned up the documentation

--- src/ttyrec/io.py
+++ src/ttyrec/io.py
@@ -12,7 +12,7 @@ _HEADER = '<lli'
     int length
 
 sec: seconds since epoch
-usec: microseconds since last second
+usec: microseconds (since last second :-)
 length: length of the following payload in bytes.
 """
 
@@ -50,11 +50,38 @@ class Options(dict):
         return ','.join(values)     
     
 class TTYrecStream(object):
+    """This objects encapsulates all handling of ttyrec files and their ascii representation.
+It works also as a generator so you can iterate the results as may times as required.
+The operations applied are stored and reapplied every time the generator is run.
+This means that no access is being done until required, each element is accessed only once
+and on demand.
+All methods return the object itself to easy concatenation. This is an example::
+
+    from ttyrec.io import TTYrecStream
+    
+    #load a file and add some intro (file is not being read at this time)
+    basic = TTYrecStream.load_ttyrec('/tmp/a_file').add_intro(intro_delay=2)
+    
+    #print the first five entries without ever accessing all the file
+    import itertools
+    print list(itertools.islice(basic, 0 , 5))
+    
+    #continue processing
+    basic.mark_input().delay_input(delay_after_input=2.5)
+    
+    #write the file without every getting all the file to memory
+    basic.write_ascii('/tmp/ascii_repr')
+
+There is no buffer strategy in place besides the default system stratgegy for handling the "open" call.
+defining this would be very simple. 
+"""
     def __init__(self):
+        """Prepare the process pipe and the empty generator"""
         self._process_pipe = []
         self._gen = None
 
     def __store(self):
+        """Store the method being called for re-play."""
         frame = currentframe(1)
         arg_spec = getargvalues(frame)
         name = frame.f_code.co_name
@@ -66,15 +93,23 @@ class TTYrecStream(object):
         return self
     
     def __reload(self):
+        """Reloads the configuration to setup the generator again."""
         proc_pipe = self._process_pipe
         self._process_pipe = []
         for method, args in proc_pipe:
             getattr(self,method)(**args)
-            
+
+    def __iter__(self):
+        """Returns the setup iterator and prepare a new one"""
+        it = self._gen
+        self.__reload()
+        return it
+
     def read_ttyrec(self, tty_file):
-        """:param tty_file: ttyrecord binary input file.
-    :returns: a generator for a list of (timestamp, payload) values
-    """
+        """Reads a ttyrec binary file.
+
+:param tty_file: ttyrecord binary input file.
+:returns: This object"""
         def gen():
             with open(tty_file, 'rb') as fin:
                 try:
@@ -85,14 +120,17 @@ class TTYrecStream(object):
                         #ready
                         dt_stamp = to_datetime(seconds=sec, microseconds=usec)
                         
-                        yield (dt_stamp, payload, Options())
+                        yield dt_stamp, payload, Options()
                 except:
                     pass
         self._gen = gen()
         return self.__store()
     
     def read_ascii(self, ascii_file):
-        """:param ascii_file: ascii input file."""
+        """Reads an ascii file representing a ttyrec binary.
+
+:param ascii_file: ascii input file.
+:returns: This object"""
         def gen():
             entry_nr=0
             line_nr=0
@@ -125,7 +163,7 @@ class TTYrecStream(object):
                         line_nr += payload.count('\n')
                             
                         yield dt_stamp, payload, options
-                        assert(fin.read(1)=='\n') #this should be a carriage return
+                        assert(fin.read(1)=='\n') #There should be a carriage return separating each entry
                         line_nr +=1
                 except:
                     print "Error in entry %s (line~%s): %s" % (entry_nr, line_nr, line)
@@ -134,8 +172,9 @@ class TTYrecStream(object):
         return self.__store()
     
     def write_ascii(self, ascii_file):
-        """:param tty_file: ttyrecord binary input file.
-    :param ascii_file: ascii output file.
+        """Writes result to an ascii file.
+
+:param ascii_file: ascii output file.
     """
         last_time = None
         if isinstance(ascii_file, basestring):
@@ -162,7 +201,26 @@ class TTYrecStream(object):
         return self
         
     def write_ttyrec(self, tty_file):
-        """:param tty_file: ttyrecord binary output file."""
+        """Writes result to a ttyrec binary file that can be replayed with ttyplay.
+        
+:param tty_file: ttyrecord binary output file."""
+        
+        from random import Random
+        r = Random(0)
+        def jitter_func(time, jitter=None, max_delay=None, cap_to_max=None):
+            if jitter is None: jitter = 0.02
+            if max_delay is None: max_delay = 1
+            if cap_to_max is None: cap_to_max = True
+            res = r.random()
+            if res < 0.5:
+                return time * (res * 2 * jitter + (1-jitter))
+            elif time > max_delay:
+                if cap_to_max: return max_delay
+                else: return time
+            else:
+                res = (res - 0.5) * 2 * jitter
+                return (max_delay - time) * res + time
+
         with open(tty_file , 'wb') as fout:
             offset = timedelta()
             for dt_stamp, payload, options in self._gen:
@@ -170,24 +228,27 @@ class TTYrecStream(object):
                 if options:
                     if 'i' in options:
                         secs = options['i']
-                        if secs is None:
-                            secs = 0.1
-                        else:
-                            secs = float(secs)
-                        step = timedelta(microseconds=int(secs * 1000000))
+                        if secs is None: secs = 0.1
+                        else: secs = float(secs)
+                        jitter = options.get('j', None)
+                        if jitter is not None: jitter = float(jitter)
+                        norm_char_time = timedelta(seconds=secs)
+                        
                         #to accommodate first step
-                        step_stamp = dt_stamp - step    
+                        step = timedelta(0)
+                        step_stamp = dt_stamp    
                         #this is input we must extend it in a typewritter similar manner
                         for c in payload:
                             step_stamp += step
-                            sec, usec = to_timestamp_tuple(step_stamp)
+                            sec, usec = to_timestamp_tuple(step_stamp + offset)
                             header = pack(_HEADER, sec, usec, 1)
                             fout.write(header)
                             fout.write(c)
+                            step = to_timedelta(jitter_func(to_timestamp(norm_char_time), jitter))
                         offset += (step_stamp - dt_stamp)
                         #this is already processed, skip
                         continue
-                sec, usec = to_timestamp_tuple(dt_stamp)
+                sec, usec = to_timestamp_tuple(dt_stamp + offset)
                 header = pack(_HEADER, sec, usec, length)
                 
                 fout.write(header)
@@ -198,44 +259,10 @@ class TTYrecStream(object):
         return self
         
     #EFFECTS
-    def norm_input(self, cpm=350, jitter=0.2, max_delay=1, cap_to_max=False):
-        """Normalizes input to the given CPM (characters per minute) speed.
-    :param tty_list: a ttyrec list.
-    :param cpm: characters per minute for the desired output."""
-        def gen(old_generator):
-            norm_char_time = timedelta(microseconds=60.0 * 1000000.0 / cpm)
-            from random import Random
-            r = Random(0)
-            def jitter_func(time):
-                res = r.random()
-                if res < 0.5:
-                    return time * (res * 2 * jitter + (1-jitter))
-                elif time > max_delay:
-                    if cap_to_max: return max_delay
-                    else: return time
-                else:
-                    res = (res - 0.5) * 2 * jitter
-                    return (max_delay - time) * res + time
-            
-            last_tstamp = None
-            offset = timedelta()
-            for tstamp, payload, options in old_generator:
-                if last_tstamp and len(payload) == 1:
-                    char_time = to_timedelta(jitter_func(to_timestamp(norm_char_time)))
-                    #this is the type of input we care for normalization
-                    offset += char_time - (tstamp - last_tstamp)
-                
-                yield tstamp + offset, payload, options
-                last_tstamp = tstamp
-                
-        self._gen = gen(self._gen)
-        return self.__store()
-    
     def cap_delays(self, max_delay=3):
         """Reduces all delay to the given maximum.
     
-    :param tty_list: a ttyrec list.
-    :param max_delay: maximal number of seconds to wait between any kind of feedback (i.e. input or output)."""
+:param max_delay: maximal number of seconds to wait between any kind of feedback (i.e. input or output)."""
         def gen(old_generator):
             last_tstamp = datetime.now()
             offset = timedelta()
@@ -252,8 +279,7 @@ class TTYrecStream(object):
     def change_speed(self, speed=1):
         """changes the recorded speed to the given one.
     
-    :param tty_list: a ttyrec list.
-    :param speed: speed factor (<1 = slower, >1 = faster)."""
+:param speed: speed factor (<1 = slower, >1 = faster)."""
         def gen(old_generator):
             last_tstamp = None
             offset = timedelta()
@@ -270,8 +296,7 @@ class TTYrecStream(object):
     def add_intro(self, intro_delay=1):
         """Clears the screen before starting and remain like that for a while.
     
-    :param tty_list: a ttyrec list.
-    :param intro_delay: number of seconds (or fraction) to remain with the screen black."""
+:param intro_delay: number of seconds (or fraction) to remain with the screen black."""
         def gen(old_generator):
             show_intro = True
             clear_screen = '\x1b[H\x1b[2J'
@@ -288,8 +313,7 @@ class TTYrecStream(object):
     def delay_lines(self, delay_per_line=0.1):
         """Break lines stored and show them with some delay, line by line.
         
-    :param tty_list: a ttyrec list.
-    :param delay_per_line: seconds (or fraction) to wait between lines"""
+:param delay_per_line: seconds (or fraction) to wait between lines"""
         def gen(old_generator):
             delay = timedelta(microseconds=delay_per_line * 1000000)
             offset = timedelta()
@@ -305,11 +329,10 @@ class TTYrecStream(object):
         return self.__store()
     
     def delay_input(self, delay_before_input=0, delay_after_input=1):
-        """Adds some delay before nand/or after the input is done.
+        """Adds some delay before and/or after the input is done.
         
-    :param tty_list: a ttyrec list.
-    :param delay_before_input: seconds (or fraction) to wait before input starts.
-    :param delay_after_input: seconds (or fraction) to wait after input finishes."""
+:param delay_before_input: seconds (or fraction) to wait before input starts.
+:param delay_after_input: seconds (or fraction) to wait after input finishes."""
         def gen(old_generator):
             delay_before = timedelta(microseconds=delay_before_input * 1000000)
             delay_after = timedelta(microseconds=delay_after_input * 1000000)
@@ -335,6 +358,7 @@ class TTYrecStream(object):
         return self.__store()
     
     def merge_lines(self, threshold=0.01):
+        """Merge lines with less than threshold seconds pause together."""
         def gen(old_generator):
             last_entry = [to_datetime(0), None, None]
             carry = False
@@ -357,17 +381,18 @@ class TTYrecStream(object):
         return self.__store()
     
     def mark_input(self, prompt_suffix=' $ '):
+        """Mark lines following what is defined to be the end of the prompt as input 
+(if not already marked as such)"""
         def gen(old_generator):
             next_is_input = False
             for dt_stamp, payload, options in old_generator:
                 if next_is_input:
                     next_is_input = False
-                    options.add('i')
+                    if 'i' not in options: options.add('i')
                 elif prompt_suffix and payload.endswith(prompt_suffix):
                     next_is_input = True
                 yield dt_stamp, payload, options
         self._gen = gen(self._gen)
         return self.__store()
         
-    
         
